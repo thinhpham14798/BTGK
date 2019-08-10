373203844f9fc97825da08e6d6027fd59210c0fe
thinhpham14798 <thinhpham14798@gmail.com>
2019-08-10 21:26:51 +0700

    /~.~/  Hoàn tất chức năng thêm User

--- Beginning/Users.aspx
+++ Beginning/Users.aspx
@@ -4,11 +4,28 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+    <title>Thêm thành viên</title>
 </head>
 <body>
     <form id="form1" runat="server">
         <div>
+            <div style="width: 300px; margin: 0 auto;  border: groove; padding: 10px;">
+                <table>
+                    <tr>
+                        <td><label>Tên đăng nhập:</label></td>
+                        <td><asp:TextBox runat="server" ID="txtUserName"></asp:TextBox></td>
+                    </tr>
+                    <tr>
+                        <td><label>Mật khẩu:</label></td>
+                        <td><asp:TextBox runat="server" ID="txtPassword" TextMode="Password"></asp:TextBox></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><asp:Button ID="btnThem" runat="server" Text="Thêm" OnClick="btnThem_Click"/></td>
+                    </tr>
+                </table>
+                
+            </div>
         </div>
     </form>
 </body>
--- Beginning/Users.aspx.cs
+++ Beginning/Users.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,6 +13,33 @@ namespace Beginning
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnThem_Click(object sender, EventArgs e)
+        {
+            SqlConnection con;
+            con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|DOTNET.mdf;Database=DOTNET;Integrated Security=True");
+            try
+            {
+                con.Open();
+                String sql = "Insert Into Users Values('" + txtUserName.Text + "', '" + txtPassword.Text + "')";
+                SqlCommand com = new SqlCommand(sql, con);
+                try
+                {
+                    com.ExecuteNonQuery();
+                    Response.Write("Đã thêm !!!!");
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("Đã tồn tại username này trong hệ thống!!!!!!!!!");
+                }
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Lỗi kết nối tới CSDL");
+            }
+            
         }
     }
 }
